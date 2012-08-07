8bf1745aec388dcf9b5a5ab64a89b7a46ade2613
Ruben Jenster <r@j5r.eu>
2012-08-07 14:26:33 +0200

    Added required prototype unique function back

--- zep.js
+++ zep.js
@@ -1,3 +1,19 @@
+Array.prototype.unique = function () {
+    var r = new Array();
+    o:for(var i = 0, n = this.length; i < n; i++)
+    {
+        for(var x = 0, y = r.length; x < y; x++)
+        {
+            if(r[x]==this[i])
+            {
+                continue o;
+            }
+        }
+        r[r.length] = this[i];
+    }
+    return r;
+};
+
 function extractDays() {
 
     var day_values = [];
